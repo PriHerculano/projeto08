841996180b6ddbf92888212ecc13421a6eb790d1
Priscila Herculano <127545294+PriHerculano@users.noreply.github.com>
2023-05-09 02:15:29 -0300

    Add files via upload

---
+++ projeto08/projeto8.1/js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos:[
+        {
+            numero:'38111',
+            nome: 'Justin Bieber',
+            partido: 'ABC',
+            fotos:[
+            {url:'jb.jpg', legenda:'Vereador', small: false}
+            ]
+        },
+        {
+            numero:'77222',
+            nome: 'Shawn Mendes',
+            partido: 'DEFG',
+            fotos:[
+            {url:'sm.jpg', legenda:'Vereador', small: false}
+            ]
+        },
+        ]
+},
+{
+    titulo: 'PREFEITO',
+    numeros:2,
+    candidatos:[
+        {
+            numero: '99',
+            nome: 'Ciclano',
+            partido: 'ABC',
+            vice: 'Cic',
+            fotos:[
+                {url: 'coldplay.jpg', legenda: 'Prefeito'},
+                {url: 'coldplayvocal.jpg', legenda: 'Vice-prefeito', small: true}
+            ]
+        },
+        {
+            numero: '84',
+            nome: 'Zulano',
+            partido: 'QWRTY',
+            vice: 'Zul',
+            fotos:[
+                {url: 'imaginedragons.jpg', legenda: 'Prefeito'},
+                {url: 'imaginedragonsvocal.jpg', legenda: 'Vice-prefeito', small: true}
+            ]
+        },
+    ]
+}
+];
